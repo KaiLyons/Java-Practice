6d3e66e77da599c6735296a61ab7b831ddf451ff
Safyre <lyons.safyre@gmail.com>
2019-09-23 15:28:18 -0600

    Create GetName.java

---
+++ GetName.java
@@ -0,0 +1,13 @@
+package start;
+import java.util.*;
+
+public class GetName {
+
+	public static void main(String[] args) {
+		Scanner inp = new Scanner(System.in);
+		System.out.println("Hello! What's Your name?");
+		String name = inp.next();
+		System.out.println("Why hello there, " + name);
+	}
+
+}
